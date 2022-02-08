ce7f366320d8e9453633656f476043f41d56fd18
Lachlan Kemp <me@lachlankemp.com>
2022-02-08 21:55:18 +1030

    test

--- scrape.js
+++ scrape.js
@@ -1,28 +1,29 @@
-require('dotenv').config();
-const puppeteer = require('puppeteer')
-const fastify = require('fastify')({
-  logger: true
-})
+import dotenv from 'dotenv'
+import puppeteer from 'puppeteer'
+import Fastify from 'fastify'
+
+dotenv.config()
+
+const fastify = Fastify({ logger: true })
 const username = process.env.EMBERPULSE_USERNAME
 const password = process.env.EMBERPULSE_PASSWORD
+console.log(`Using node: ${process.version}`)
+
 async function scrape() {
-  const browser = await puppeteer.launch({
-		'args' : [
-			'--no-sandbox',
-			'--disable-setuid-sandbox'
-		]
-	});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({width: 1200, height: 720});
+	console.info("Loading Emberpulse...")
+	console.log("Authenticating with: " + username)
   await page.goto('https://emberpulse.com.au/login', { waitUntil: 'networkidle0' }); // wait until page load
+	console.info("Authenticating...")
   await page.type('#username', username);
   await page.type('#password', password);
 
   // click and wait for navigation
-  await Promise.all([
-    page.click('[type="submit"]'),
-    page.waitForNavigation({ waitUntil: 'networkidle0' }),
-  ]);
+	page.click('[type="submit"]')
+	console.info("Awaiting Data...")
+	await page.waitForNavigation({ waitUntil: 'networkidle0' })
 	await page.waitForSelector('#home-icon-readout')
 
 	// Power Data
@@ -39,15 +40,16 @@ async function scrape() {
 		importing: gridDirection !== 'exporting' ? grid : 0
 	})
 }
-fastify.get('/data', async function (request, reply) {
-  reply.send(await scrape())
-})
 
-// Run the server!
-fastify.listen(3000, function (err, address) {
-  if (err) {
+fastify.get('/emberpulse', async (request, reply) => {
+  return await scrape()
+})
+const start = async () => {
+  try {
+    await fastify.listen(80)
+  } catch (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-  // Server is now listening on ${address}
-})
+}
+start()
